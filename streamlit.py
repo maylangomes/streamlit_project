bffb801d02cacec0d97c302b12ceb3c06cb2d360
Maylan <maylan.gomes@epitech.eu>
2024-06-12 16:12:23 +0200

    maj

--- streamlit.py
+++ streamlit.py
@@ -9,3 +9,26 @@ st.set_page_config(page_title="Superstore", page_icon=":bar_chart:", layout="wid
 
 st.title(" :bar_chart: Sample SuperStore EDA")
 st.markdown('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allow_html=True)
+
+fl = st.file_uploader(":file_folder: Upload a file", type=(["csv", "txt", "xlsx", "xls"]))
+if fl is not None:
+    filename = fl.name
+    st.write(filename)
+    df = pd.read_csv(filename)
+else:
+    os.chdir(r"/home/maylan/python/streamlit_deploy")
+    df = pd.read_csv("Superstore.csv")
+
+col1, col2 = st.columns((2))
+df["Order Date"] = pd.to_datetime(df["Order Date"])
+
+startDate = pd.to_datetime(df["Order Date"]).min()
+endDate = pd.to_datetime(df["Order Date"]).max()
+
+with col1:
+    date1 = pd.to_datetime(st.date_input("Start Date", startDate))
+
+with col2:
+    date2 = pd.to_datetime(st.date_input("End Date", endDate))
+
+df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
