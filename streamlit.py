b199e32e0371d4e8c772a89a882a3aec9420f996
Maylan <maylan.gomes@epitech.eu>
2024-06-12 15:05:10 +0200

    test

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import plotly.express as px
+import pandas as pd
+import os
+import warnings
+warnings.filterwarnings('ignore')
 
-st.write('Hello world')
+st.set_page_config(page_title="Superstore", page_icon=":bar_chart:", layout="wide")
+
+st.title(" :bar_chart: Sample SuperStore EDA")
+st.markdown('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allow_html=True)
