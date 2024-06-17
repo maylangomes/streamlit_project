536f85d38fa7365c3207e1e1cbbcb1d948bf82a1
Maylan <maylan.gomes@epitech.eu>
2024-06-18 01:49:47 +0200

    feat: adaptation de la logique pour les décès par pourcentage

--- streamlit.py
+++ streamlit.py
@@ -1,141 +1,104 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-import os
 import warnings
+import plotly.figure_factory as ff
+
 warnings.filterwarnings('ignore')
 
-st.set_page_config(page_title="Superstore", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="Analyse des décès par département", page_icon=":chart_with_upwards_trend:", layout="wide")
 
-st.title(" :bar_chart: Sample SuperStore EDA")
+st.title(":bar_chart: Analyse des décès par département en France")
 st.markdown('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allow_html=True)
 
-fl = st.file_uploader(":file_folder: Upload a file", type=(["csv", "txt", "xlsx", "xls"]))
+fl = st.file_uploader(":file_folder: Upload a file", type=(["csv"]))
 if fl is not None:
     filename = fl.name
     st.write(filename)
-    df = pd.read_csv(filename)
+    df = pd.read_csv(fl, delimiter=';', encoding='ISO-8859-1')
 else:
-    df = pd.read_csv("Superstore.csv")
+    df = pd.read_csv("deces.csv", delimiter=';', encoding='utf-8')
+
+# Renommer les colonnes pour une manipulation plus facile
+df.columns = ["Zone", "Département", "Total_Deces2023", "Total_0_24ans_Deces2023", "Total_25_49ans_Deces2023",
+       "Total_50_64ans_Deces2023", "Total_65_74ans_Deces2023", "Total_75_84ans_Deces2023", "Total_85ans_plus_Deces2023",
+       "Total décès 2022", "0-24 ans", "25-49 ans", "50-64 ans",
+       "65-74 ans", "75-84 ans", "85 ans et plus", "Total_Deces2019",
+       "Total_0_24ans_Deces2019", "Total_25_49ans_Deces2019", "Total_50_64ans_Deces2019", "Total_65_74ans_Deces2019",
+       "Total_75_84ans_Deces2019", "Total_85ans_plus_Deces2019", "Date_evenement", "Population"]
+
+# Garder uniquement les colonnes nécessaires pour 2022
+df = df[["Zone", "Département", "Total décès 2022", "0-24 ans", "25-49 ans",
+         "50-64 ans", "65-74 ans", "75-84 ans",
+         "85 ans et plus", "Population"]]
+
+# Remplir les valeurs manquantes avec 'Unknown'
+df.fillna('Unknown', inplace=True)
+
+st.sidebar.header("Choisissez vos filtres : ")
+regions = st.sidebar.multiselect("Choisissez votre région", df["Département"].unique())
+
+if not regions:
+    filtered_df = df
+else:
+    filtered_df = df[df["Département"].isin(regions)]
+
+# Ajouter une colonne pour le pourcentage de décès par rapport à la population
+filtered_df["% de décès"] = (filtered_df["Total décès 2022"] / filtered_df["Population"]) * 100
+
+# Analyse des décès par zone
+deces_par_zone = filtered_df.groupby(by=["Département"], as_index=False).agg(
+    {"Total décès 2022": "sum", "Population": "first", "% de décès": "mean"})
 
 col1, col2 = st.columns((2))
-df["Order Date"] = pd.to_datetime(df["Order Date"])
-
-startDate = pd.to_datetime(df["Order Date"]).min()
-endDate = pd.to_datetime(df["Order Date"]).max()
-
 with col1:
-    date1 = pd.to_datetime(st.date_input("Start Date", startDate))
+    st.subheader("Total des décès par zone")
+    fig = px.bar(deces_par_zone, x="Département", y="Total décès 2022", text="Total décès 2022", template="seaborn")
+    st.plotly_chart(fig, use_container_width=True, height=400)
 
 with col2:
-    date2 = pd.to_datetime(st.date_input("End Date", endDate))
-
-df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
-
-st.sidebar.header("Choose your filter: ")
-region = st.sidebar.multiselect("Pick your region", df["Region"].unique())
-
-if not region:
-    df2 = df.copy()
-else:
-    df2 = df[df["Region"].isin(region)]
-
-state = st.sidebar.multiselect("Pick the State", df2["State"].unique())
-if not state:
-    df3 = df2.copy()
-else:
-    df3 = df2[df2["State"].isin(state)]
-
-city = st.sidebar.multiselect("Pick the city", df3["City"].unique())
-
-if not region and not state and not city:
-    filtered_df = df
-elif not state and not city:
-    filtered_df = df[df["Region"].isin(region)]
-elif not region and not city:
-    filtered_df = df[df["State"].isin(state)]
-elif state and city:
-    filtered_df = df3[df["State"].isin(state) & df3["City"].isin(city)]
-elif region and city:
-    filtered_df = df3[df["Region"].isin(region) & df3["City"].isin(city)]
-elif region and state:
-    filtered_df = df3[df["Region"].isin(region) & df3["State"].isin(state)]
-elif city:
-    filtered_df = df3[df3["City"].isin(city)]
-else:
-    filtered_df = df3[df3["Region"].isin(region) & df3["State"].isin(state) & df3["City"].isin(city)]
-
-category_df = filtered_df.groupby(by = ["Category"], as_index = False)["Sales"].sum()
-
-with col1:
-    st.subheader("Category wise Sales")
-    fig = px.bar(category_df, x = "Category", y = "Sales", text = ['${:,.2f}'.format(x) for x in category_df["Sales"]],
-                 template = "seaborn")
-    st.plotly_chart(fig, use_container_width = True, height = 200)
-
-with col2:
-    st.subheader("Region wise Sales")
-    fig = px.pie(filtered_df, values = "Sales", names = "Region", hole = 0.5)
-    fig.update_traces(text = filtered_df["Region"], textposition = "outside")
-    st.plotly_chart(fig, use_container_width = True)
+    st.subheader("Répartition des décès par zone")
+    fig = px.pie(filtered_df, values="Total décès 2022", names="Département", hole=0.5)
+    fig.update_traces(text=filtered_df["Département"], textposition="outside")
+    st.plotly_chart(fig, use_container_width=True)
 
 cl1, cl2 = st.columns(2)
 with cl1:
-    with st.expander("Category_Viewdata"):
-        st.write(category_df.style.background_gradient(cmap="Blues"))
-        csv = category_df.to_csv(index = False).encode('utf-8')
-        st.download_button("Download Data", data = csv, file_name = "Category.csv", mime = "text/csv",
-                            help = "Click here to download the data as a CSV file")
-        
+    with st.expander("Données par zone"):
+        st.write(deces_par_zone.style.background_gradient(cmap="Blues"))
+        csv = deces_par_zone.to_csv(index=False).encode('utf-8')
+        st.download_button("Télécharger les données", data=csv, file_name="Total_Deces_par_Zone.csv", mime="text/csv",
+                          help="Cliquez ici pour télécharger les données au format CSV")
+
 with cl2:
-    with st.expander("Region_Viewdata"):
-        region = filtered_df.groupby(by = "Region", as_index = False)["Sales"].sum()
-        st.write(region.style.background_gradient(cmap="Oranges"))
-        csv = region.to_csv(index = False).encode('utf-8')
-        st.download_button("Download Data", data = csv, file_name = "Region.csv", mime = "text/csv",
-                            help = "Click here to download the data as a CSV file")
-        
-filtered_df["month_year"] = filtered_df["Order Date"].dt.to_period("M")
-st.subheader("Time Series Analysis")
+    with st.expander("Données détaillées"):
+        st.write(filtered_df.style.background_gradient(cmap="Oranges"))
+        csv = filtered_df.to_csv(index=False).encode('utf-8')
+        st.download_button("Télécharger les données", data=csv, file_name="Donnees_detaillees.csv", mime="text/csv",
+                          help="Cliquez ici pour télécharger les données au format CSV")
 
-linechart = pd.DataFrame(filtered_df.groupby(filtered_df["month_year"].dt.strftime("%Y : %b"))["Sales"].sum()).reset_index()
-fig2 = px.line(linechart, x="month_year", y="Sales", labels = {"Sales": "Amount"}, height=500, width=1000, template="gridon")
-st.plotly_chart(fig2, use_container_width=True)
-
-with st.expander("View Data of TimeSeries:"):
-    st.write(linechart.T.style.background_gradient(cmap="Blues"))
-    csv = linechart.to_csv(index=False).encode("utf-8")
-    st.download_button("Download Data", data = csv, file_name = "TimeSeries.csv", mime = "text/csv")
-
-# create treem
-st.subheader("Hierarchical view of Sales using TreeMap")
-fig3 = px.treemap(filtered_df, path = ["Region", "Category", "Sub-Category"], values = "Sales", hover_data = ["Sales"],
-                  color = "Sub-Category")
-fig3.update_layout(width = 800, height = 650)
-st.plotly_chart(fig3, use_container_width = True)
+st.subheader("Vue hiérarchique des décès par zone")
+fig3 = px.treemap(filtered_df, path=["Département"], values="Total décès 2022", hover_data=["Total décès 2022", "% de décès"], color="Total décès 2022")
+fig3.update_layout(width=800, height=650)
+st.plotly_chart(fig3, use_container_width=True)
 
 chart1, chart2 = st.columns((2))
 with chart1:
-    st.subheader("Segment wise Sales")
-    fig = px.pie(filtered_df, values = "Sales", names = "Segment", template = "plotly_dark")
-    fig.update_traces(text = filtered_df["Segment"], textposition = "inside")
-    st.plotly_chart(fig, use_container_width = True)
+    st.subheader("Répartition des décès par groupe d'âge")
+    fig = px.bar(filtered_df, x="Département", y=["0-24 ans", "25-49 ans", "50-64 ans",
+                                                 "65-74 ans", "75-84 ans", "85 ans et plus"],
+                 barmode="stack", template="gridon")
+    st.plotly_chart(fig, use_container_width=True)
 
-with chart2:
-    st.subheader("Category wise Sales")
-    fig = px.pie(filtered_df, values = "Sales", names = "Category", template = "gridon")
-    fig.update_traces(text = filtered_df["Category"], textposition = "inside")
-    st.plotly_chart(fig, use_container_width = True)
+st.subheader(":point_right: Tableau récapitulatif des décès")
+with st.expander("Tableau récapitulatif"):
+    df_sample = df[0:5][["Département", "Total décès 2022", "0-24 ans", "25-49 ans",
+                         "50-64 ans", "65-74 ans", "75-84 ans", "85 ans et plus", "Population", "% de décès"]]
+    fig = ff.create_table(df_sample, colorscale="Cividis")
+    st.plotly_chart(fig, use_container_width=True)
 
-import plotly.figure_factory as ff
-
-st.subheader(":point_right: Month wise Sub-Category Sales Summary")
-with st.expander("Summary_Table"):
-    df_sample = df[0:5][["Region", "State", "City", "Category", "Sales", "Profit", "Quantity"]]
-    fig = ff.create_table(df_sample, colorscale = "Cividis")
-    st.plotly_chart(fig, use_container_width = True)
-
-    st.markdown("Month wise sub-Category Table")
-    filtered_df["month"] = filtered_df["Order Date"].dt.month_name()
-    sub_category_Year = pd.pivot_table(data = filtered_df, values = "Sales", index = ["Sub-Category"], columns = "month")
-    st.write(sub_category_Year.style.background_gradient(cmap = "Blues"))
+    st.markdown("Détails des décès par zone")
+    details_deces_zone = pd.pivot_table(data=filtered_df, values=["Total décès 2022", "0-24 ans", "25-49 ans",
+                                                                  "50-64 ans", "65-74 ans", "75-84 ans", "85 ans et plus", "Population", "% de décès"],
+                                        index=["Département"], aggfunc="sum")
+    st.write(details_deces_zone.style.background_gradient(cmap="Blues"))
